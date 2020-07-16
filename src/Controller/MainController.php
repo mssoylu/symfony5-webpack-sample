6284beeee26d8ab33454bd86345997e3e3d7c2a7
mssoylu <mehmet@soylu.net>
2020-07-16 12:05:36 +0300

    some fix

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,18 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Content;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class MainController extends AbstractController
 {
-    /**
-     * @Route("/main", name="main")
-     */
     public function index()
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $contents = $em->getRepository(Content::class)->findAll();
+
         return $this->render('main/index.html.twig', [
-            'controller_name' => 'MainController',
+            'contents' => $contents
         ]);
     }
+
+    public function show($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $content = $em->getRepository(Content::class)->find($id);
+
+        return $this->render('main/show.html.twig', [
+            'content' => $content
+        ]);
+    }
+
 }

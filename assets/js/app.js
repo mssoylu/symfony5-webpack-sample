0f0fa1414833d766b4fa5c306c3adee9a3d1b086
mssoylu <mehmet@soylu.net>
2020-07-16 17:57:45 +0300

    up

---
+++ assets/js/app.js
@@ -0,0 +1,18 @@
+import '../css/app.css';
+require('@fortawesome/fontawesome-free/css/all.min.css');
+require('@fortawesome/fontawesome-free/js/all.js');
+
+// app.js
+
+const $ = require('jquery');
+require('bootstrap');
+
+// require the JavaScript
+// require the JavaScript
+require('bootstrap-star-rating');
+// require 2 CSS files needed
+require('bootstrap-star-rating/css/star-rating.css');
+require('bootstrap-star-rating/themes/krajee-fa/theme.min.js');
+require('bootstrap-star-rating/themes/krajee-fa/theme.css');
+
+console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
